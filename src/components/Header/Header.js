3929121e346ed2c653c76f6af932d04af64b932a
AsifKhan-27 <asifak001@gmail.com>
2022-09-29 18:01:48 +0600

    added logo and title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../../images/bnw.jpg';
 import './Header.css';
 
 const Header = () => {
     return (
         <nav className="header">
-            {/* <img src={logo} alt=""/> */}
+            <img className='logo' src={logo} alt=""/>
             <div>
                 <a href="/shop">Movie Club</a>
             </div>
